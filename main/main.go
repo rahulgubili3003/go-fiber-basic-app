34c559fb4f11d6362280cc5ab111092e9506260a
rahulgubili <rahul.gubiligari@paypay-corp.co.jp>
2024-05-13 03:29:07 +0900

    Go fiber basic app

---
+++ main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello, we are using Go Fiber!!",
+		})
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
